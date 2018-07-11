22d6ea506f95a8e8e650b0c81c27432c2b4a24fb
Akhil Bagaria <abagaria@apple.com>
2018-07-11 15:57:22 -0700

    Cleaned up Abstract Grid World Policy Generators somewhat so that they take in grounded_tasks as AbstractTasks instead of strings

--- simple_rl/AMDPs/AbstractGridWorld/AGWPolicyGenerators.py
+++ simple_rl/AMDPs/AbstractGridWorld/AGWPolicyGenerators.py
@@ -3,17 +3,13 @@ from simple_rl.AMDPs.AMDPPolicyGeneratorClass import AMDPPolicyGenerator
 from simple_rl.tasks.four_room.FourRoomMDPClass import FourRoomMDP
 from simple_rl.AMDPs.AbstractGridWorld.GridWorldAMDPClass import FourRoomL1MDP, FourRoomL1State
 
-import re
-
 class L1PolicyGenerator(AMDPPolicyGenerator):
     def __init__(self, l0MDP, verbose=False):
         self.domain = l0MDP
         self.verbose = verbose
 
-    def generatePolicy(self, l1_state, grounded_action_str):
-        grounded_action = FourRoomL1GroundedAction(grounded_action_str)
+    def generatePolicy(self, l1_state, grounded_action):
         mdp = FourRoomL1MDP(l1_state.agent_in_room_number, grounded_action.goal_state.agent_in_room_number)
-
         return self.getPolicy(mdp)
 
     # TODO: Figure out a way to project l0 state to l1 w/o having access to l0Domain in l1
@@ -29,8 +25,7 @@ class L0PolicyGenerator(AMDPPolicyGenerator):
 
     def generatePolicy(self, state, grounded_task):
         # TODO: There should be a better way to get the destination_room from the grounded_task (create GT class)
-        destination_room = int(re.findall(r'\d+', grounded_task)[0])
-        destination_location = self.domain._get_single_location_for_room(destination_room)
+        destination_location = self.domain._get_single_location_for_room(grounded_task.goal_state.agent_in_room_number)
         init_location = (state.x, state.y)
         mdp = FourRoomMDP(self.domain.width, self.domain.height, init_loc=init_location, goal_locs=[destination_location])
         return self.getPolicy(mdp)
