05af4d04109cf9583d10c9b9f7fed174846a4710
Akhil Bagaria <abagaria@apple.com>
2018-07-10 20:08:41 -0700

    Cleaned up AMDPAgent so that it operates solely on abstract data structures so that it is in no way tied to the AbstractFourRoomMDP

--- simple_rl/AMDPs/AMDPSolverClass.py
+++ simple_rl/AMDPs/AMDPSolverClass.py
@@ -1,11 +1,18 @@
 from simple_rl.mdp.StateClass import State
+from simple_rl.mdp.MDPClass import MDP
+from simple_rl.AMDPs.TaskNodesClass import AbstractTask, RootTaskNode
 
 from collections import defaultdict
-import re
-import pdb
 
 class AMDPAgent(object):
     def __init__(self, root_grounded_task, policy_generators, base_mdp):
+        '''
+        AbstractMDP solver class
+        Args:
+            root_grounded_task (RootTaskNode)
+            policy_generators (list) of type objects (one for each level below the root)
+            base_mdp (MDP): Lowest level environment MDP
+        '''
         self.root_grounded_task = root_grounded_task
         self.policy_generators = policy_generators
         self.base_mdp = base_mdp
@@ -15,52 +22,63 @@ class AMDPAgent(object):
             self.state_stack.append(State())
             self.policy_stack.append(defaultdict())
         self.max_level = len(self.policy_generators) - 1
+        self.action_to_task_map = defaultdict()
+        self._construct_action_to_node_map(root_grounded_task)
 
     def plan(self):
         base_state = self.base_mdp.init_state
         self.state_stack[0] = base_state
 
+        # Project env MDP init state to all higher levels
         for i in range(1, len(self.policy_generators)):
             pg = self.policy_generators[i]
             base_state = pg.generateAbstractState(base_state)
             self.state_stack[i] = base_state
 
-        self.decompose(self.root_grounded_task, self.max_level)
+        # Start decomposing the highest-level task in hierarchy
+        self._decompose(self.root_grounded_task, self.max_level)
 
-    def decompose(self, grounded_task, level):
+    def _decompose(self, grounded_task, level):
+        '''
+        Ground high level tasks into environment level actions and then execute
+        in underlying environment MDP
+        Args:
+            grounded_task (AbstractTask): TaskNode representing node in task hierarchy
+            level (int): what depth we are in our AMDP task hierarchy (base MDP is l0)
+        '''
         print 'Decomposing action {} at level {}'.format(grounded_task, level)
         state = self.state_stack[level]
 
-        # TODO: Hack: Decide if this should be a string or a TaskNode
-        if not type(grounded_task) is str: grounded_task = grounded_task.name
-
-        policy = self.policy_generators[level].generatePolicy(state, grounded_task)
+        policy = self.policy_generators[level].generatePolicy(state, grounded_task.action_name)
         if level > 0:
-            while not self._is_terminal(state, grounded_task, level):
+            while not grounded_task.is_terminal(state):
                 action = policy[state]
                 self.policy_stack[level][state] = action
-                self.decompose(action, level-1)
+                self._decompose(self.action_to_task_map[action], level-1)
                 state = self.state_stack[level]
         else:
-            while not self._is_terminal(state, grounded_task, level) and not self._env_is_terminal(state):
+            while not grounded_task.is_terminal(state) and not self._env_is_terminal(state):
                 action = policy[state]
                 self.policy_stack[level][state] = action
+                print '({}, {})'.format(state, action)
                 reward, state = self.base_mdp.execute_agent_action(action)
+                print '\t--> {}'.format(state)
                 self.state_stack[level] = state
         if level < self.max_level:
             projected_state = self.policy_generators[level+1].generateAbstractState(self.state_stack[level])
             self.state_stack[level+1] = projected_state
 
-    # TODO: Hack: Somehow use TaskNodes to determine if a grounded task is terminal
-    def _is_terminal(self, state, grounded_task, level):
-        destination_room = int(re.findall(r'\d+', grounded_task)[0])
-        if level == 0:
-            destination_location = self.base_mdp._get_single_location_for_room(destination_room)
-            current_location = (int(state.x), int(state.y))
-            return  current_location == destination_location
-        elif level == 1:
-            return state.agent_in_room_number == destination_room
-        raise NotImplementedError('level {} not supported yet'.format(level))
-
     def _env_is_terminal(self, state):
-        return (state.x, state.y) in self.base_mdp.goal_locs
+        return self.base_mdp.in_goal_set(state)
+
+    def _construct_action_to_node_map(self, root_node):
+        '''
+        Use DFS to create a dictionary mapping string actions to AbstractTask nodes
+        Args:
+            root_node (AbstractTask): TaskNode representing AMDP action
+        '''
+        if root_node is not None:
+            if root_node.action_name not in self.action_to_task_map:
+                self.action_to_task_map[root_node.action_name] = root_node
+            for child_node in root_node.subtasks:
+                self._construct_action_to_node_map(child_node)
