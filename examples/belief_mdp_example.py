03e74e95a9320607a7b108dbc71b2b78494b9ba2
Akhil Bagaria <akhil_bagaria@brown.edu>
2018-08-08 10:51:01 +0530

    modify belief_mdp example file

--- examples/belief_mdp_example.py
+++ examples/belief_mdp_example.py
@@ -6,4 +6,4 @@ pomdp = Maze1DPOMDP()
 belief_mdp = BeliefMDP(pomdp)
 
 bss = BeliefSparseSampling(gen_model=belief_mdp, gamma=0.6, tol=1.0, max_reward=1.0, state=belief_mdp.init_state)
-scores, policies = bss.run(num_episodes=3, verbose=True)
+scores, policies = bss.run(verbose=True)
